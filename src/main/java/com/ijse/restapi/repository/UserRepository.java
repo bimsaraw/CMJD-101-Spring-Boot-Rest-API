b911a20a95614f45e2cde578316ac1738b9807b9
Bimsara Walallawita <bwalallawita@gmail.com>
2023-07-23 16:55:10 +0530

    Initial COmmit

---
+++ src/main/java/com/ijse/restapi/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ijse.restapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ijse.restapi.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    //Custom Queries
+}
